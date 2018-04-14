dc000824d73e24470dbe50fcd6343cb0e94dfe7e
root <root@li1582-238.members.linode.com>
2018-04-14 14:24:02 +0000

    use redis to store popular

--- kkbox2qq.js
+++ kkbox2qq.js
@@ -5,21 +5,40 @@ var redis = require("redis");
 var client = redis.createClient();
 
 var qq = require('./util/qq');
-request('https://kma.kkbox.com/charts/api/v1/daily?category=297&date=2018-04-12&lang=tc&limit=50&terr=tw&type=newrelease', function(error, response, body) {
-    body = JSON.parse(body);
-    songs = body.data.charts.newrelease;
-    for (var i = 0; i < songs.length; i++) {
-        setTimeout(function(i) {
-            songname = songs[i].song_name.split(" ")[0];
-            artistname = songs[i].artist_name.split(" ")[0];
-            qq.search(songname + ' ' + artistname, 1).then((data) => {
-                console.log(songs[i].song_id);
-                console.log(data[0]);
-                if (typeof data[0] != 'undefined') {
-                    client.set("kkbox_id:" + songs[i].song_id, data[0].id, redis.print);
-                    client.set("kkbox_id:" + songs[i].song_id, data[0].id, redis.print);
-                }
-            });
-        }, 1000 * 6 * i, i)
-    }
-});
+var db = require('./util/db');
+
+function daily_chart() {
+    client.del("popular", function(err, res) {
+        if (res == 1)
+            console.log("deleted popular");
+        request('https://kma.kkbox.com/charts/api/v1/daily?category=297&date=2018-04-12&lang=tc&limit=50&terr=tw&type=newrelease', function(error, response, body) {
+            body = JSON.parse(body);
+            songs = body.data.charts.newrelease;
+            for (var i = 0; i < songs.length; i++) {
+                setTimeout(function(i) {
+                    songname = songs[i].song_name.split(" ")[0];
+                    artistname = songs[i].artist_name.split(" ")[0];
+                    client.get("kkbox_id:" + songs[i].song_id, function(err, res) {
+                        if (res == null) {
+                            qq.search(songname + ' ' + artistname, 1).then((data) => {
+                                console.log(songs[i].song_id);
+                                console.log(data[0]);
+                                if (typeof data[0] != 'undefined') {
+                                    client.set("kkbox_id:" + songs[i].song_id, data[0].id, redis.print);
+                                    client.lpush("popular", data[0].id, redis.print);
+                                }
+                            });
+                        } else {
+                            client.lpush("popular", res, redis.print);
+                        }
+                    })
+                }, 1000 * 6 * i, i)
+            }
+        });
+    })
+}
+
+
+daily_chart();
+
+
--- util/db.js
+++ util/db.js
@@ -1,6 +1,10 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 const dbName = 'music';
+var redis = require("redis");
+var client = redis.createClient();
+const opencc = require('node-opencc');
+const he = require('he');
 
 
 const findSongById = function(id, cb) {
@@ -18,17 +22,38 @@ const findSongById = function(id, cb) {
     });
 };
 
-const findPopular = function(cb) {
+const findSongsById = function(ids, cb) {
+    ids = ids.map(Number);
     MongoClient.connect(url, function(err, client) {
         const db = client.db(dbName);
-        const collection = db.collection('popular');
-        collection.find({}).toArray(function(err, docs) {
-            if (err)
-                console.log(err);
-            client.close();
-            cb(docs);
+        const collection = db.collection('song');
+        collection.find({
+            songid: {
+                $in: ids
+            }
+        }).toArray(function(err, items) {
+            cb(items)
         })
     });
+};
+
+const findPopular = function(cb) {
+    ret = [];
+    client.lrange("popular", 1, 50, function(err, res) {
+        findSongsById(res, function(datas) {
+            datas.forEach(function(song) {
+                ret.push({
+                    "id": song.songid,
+                    "title": opencc.simplifiedToTaiwan(he.decode(song.songname)),
+                    "data": `http://139.162.98.238/data/${song.songid}_${song.songmid}.mp3`,
+                    "albumName": opencc.simplifiedToTaiwan(he.decode(song.albumname)),
+                    "albumId": song.albumid,
+                    "artistName": opencc.simplifiedToTaiwan(he.decode(song.singer[0].name))
+                })
+            })
+            cb(ret);
+        })
+    })
 };
 
 const saveLyricById = function(id, lyric, cb) {
@@ -52,5 +77,6 @@ const saveLyricById = function(id, lyric, cb) {
 module.exports = {
     findSongById,
     saveLyricById,
-    findPopular
+    findPopular,
+    findSongsById
 }
